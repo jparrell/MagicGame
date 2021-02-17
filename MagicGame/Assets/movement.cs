01f5b3777d9891b08ded4f65177db78d2eebf2a2
jparrell <44116260+jparrell@users.noreply.github.com>
2021-02-16 19:35:26 -0500

    Movement

--- MagicGame/Assets/movement.cs
+++ MagicGame/Assets/movement.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    public float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.8f;
+    private float horizontal = 0f;
+    private float vertical = 0f;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        controller = gameObject.AddComponent<CharacterController>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+ /*     if (Input.GetButton("Horizontal"))
+        {
+            horizontal += -1;
+            if (horizontal <-1)
+            {
+                horizontal = -1;
+            }
+        }
+        if (Input.GetButton("+Horizontal"))
+        {
+            horizontal += 1;
+            if (horizontal > 1)
+            {
+                horizontal = 1;
+            }
+        }
+        if (Input.GetButton("+Vertical"))
+        {
+            vertical += 1;
+            if (vertical > 1)
+            {
+                vertical = 1;
+            }
+        }
+        if (Input.GetButton("-Vertical"))
+        {
+            vertical += -1;
+            if (vertical < -1)
+            {
+                vertical = -1;
+            }
+        }*/
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        controller.Move(move * Time.deltaTime * playerSpeed);
     }
 }
