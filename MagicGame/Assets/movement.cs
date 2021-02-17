54c5680623a3ddb69fe8b9d2eb995a8bfa59a4a8
jparrell <44116260+jparrell@users.noreply.github.com>
2021-02-16 20:57:53 -0500

    Dashing

--- MagicGame/Assets/movement.cs
+++ MagicGame/Assets/movement.cs
@@ -6,12 +6,17 @@ public class movement : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
+    public float dashSpeed = 2.0f;
+    private float dashTimer= 0f;
+    public float dashCD = 2.0f;
+    private float timer = 0f;
     private bool groundedPlayer;
     public float playerSpeed = 2.0f;
     public float jumpHeight = 8.0f;
     public float gravityValue = -9.8f;
     private float horizontal = 0f;
     private float vertical = 0f;
+
 
 
 
@@ -25,6 +30,8 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+        dashTimer = timer;
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
@@ -62,14 +69,22 @@ public class movement : MonoBehaviour
                        vertical = -1;
                    }
                }*/
-        Debug.Log(Input.GetAxisRaw("Horizontal"));
+       // Debug.Log(dashTimer);
         Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
         controller.Move(move * Time.deltaTime * playerSpeed);
         if (Input.GetButton("Jump") && groundedPlayer)
         {
             playerVelocity.y += jumpHeight;
         }
+        if (Input.GetButtonDown("Dash") && dashTimer > dashCD)
+        {
+            Debug.Log("gay");
+            playerVelocity.x += (Input.GetAxisRaw("Horizontal") + dashSpeed);
+            dashTimer = 0;
+        }
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+
+
     }
 }
