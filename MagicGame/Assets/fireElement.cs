cdd6b43062d86e059006e5efaf8c14f348b94839
FTawa <65373869+FTawa@users.noreply.github.com>
2021-02-19 15:25:49 -0500

    finalelementchoosing

--- MagicGame/Assets/fireElement.cs
+++ MagicGame/Assets/fireElement.cs
@@ -7,27 +7,33 @@ public class fireElement : MonoBehaviour
 {
     public GameObject FireElementPrefab;
     public Transform ElementPoint;
-    private float firecooldown = 0f;
+    private float firePosition = 0f;
     
     void Update()
     {
-        if (firecooldown == 0 && Input.GetButtonDown("FireElement"))
+        if (firePosition == 0 && Input.GetButtonDown("FireElement"))
         {
             summonFireElement(ElementPoint.position); 
         }
-        if (firecooldown >= 2 && Input.GetButtonDown("FireElement"))
+        else if(firePosition == 1 && Input.GetButtonDown("FireElement"))
         {
-            Vector2 pos = new Vector2(ElementPoint.position.x +1, ElementPoint.position.y);
+            Vector2 pos = new Vector2(ElementPoint.position.x +1.8f, ElementPoint.position.y);
             summonFireElement(pos);
+            Debug.Log(firePosition);
         }
-        
+        else if(firePosition == 2 && Input.GetButtonDown("FireElement"))
+        {
+            Vector2 pos2 = new Vector2(ElementPoint.position.x +.9f, ElementPoint.position.y +1.2f);
+            summonFireElement(pos2);
+        }
     }   
 
     void summonFireElement(Vector2 position)
     {
+        firePosition += +1f;
         var firemana = Instantiate(FireElementPrefab, position, ElementPoint.rotation);
         firemana.transform.parent = gameObject.transform;
-        firecooldown = +1;
-        Debug.Log(firecooldown);
+        
+        //Debug.Log(firePosition);
     }
 }
