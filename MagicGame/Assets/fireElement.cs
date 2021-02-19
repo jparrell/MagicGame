254bd43ab2b0a75175bbc0906d90c4a5cd467796
FTawa <65373869+FTawa@users.noreply.github.com>
2021-02-18 22:01:47 -0500

    almost finished basics of fire

--- MagicGame/Assets/fireElement.cs
+++ MagicGame/Assets/fireElement.cs
@@ -8,18 +8,26 @@ public class fireElement : MonoBehaviour
     public GameObject FireElementPrefab;
     public Transform ElementPoint;
     private float firecooldown = 0f;
-
+    
     void Update()
     {
         if (firecooldown == 0 && Input.GetButtonDown("FireElement"))
         {
-            var firemana = Instantiate(FireElementPrefab, ElementPoint.position, ElementPoint.rotation);
-            firemana.transform.parent = gameObject.transform;
-            firecooldown = +1;      
+            summonFireElement(ElementPoint.position); 
         }
-        //if (firecooldown == 1 && Input.GetButtonDown("FireElement"))
-       // {
-            //Destroy(FireElementPrefab);
-        //}
+        if (firecooldown >= 2 && Input.GetButtonDown("FireElement"))
+        {
+            Vector2 pos = new Vector2(ElementPoint.position.x +1, ElementPoint.position.y);
+            summonFireElement(pos);
+        }
+        
     }   
+
+    void summonFireElement(Vector2 position)
+    {
+        var firemana = Instantiate(FireElementPrefab, position, ElementPoint.rotation);
+        firemana.transform.parent = gameObject.transform;
+        firecooldown = +1;
+        Debug.Log(firecooldown);
+    }
 }
